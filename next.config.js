d34d0e9e29260f4d6aaa3ddf2701dc97d4e32d9a
Totolici Alexandru <totoalex62@gmail.com>
2023-12-01 13:47:00 +0200

    1 dec v8

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
-import remarkFrontmatter from 'remark-frontmatter';
-
-export default {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          options: {
-            providerImportSource: '@mdx-js/react',
-            remarkPlugins: [remarkFrontmatter],
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
+module.exports = {
   images: {
 		domains: [
 			'localhost',
