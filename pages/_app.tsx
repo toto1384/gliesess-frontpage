7e35fb9f8a8a647f88c9a2bab34069f7b722206f
Totolici Alexandru <totoalex62@gmail.com>
2024-01-19 15:42:00 +0200

    19 jan

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MDXProvider } from '@mdx-js/react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <MDXProvider components={{
 
   }}>
-    <div>
+    <Head>
       <link href="https://app.meetfox.com/assets/styles/popup.css" rel="stylesheet" />
       <script src="https://app.meetfox.com/assets/libs/popup.min.js" type="text/javascript"></script>
+    </Head>
+    <div>
       <Component {...pageProps} />
     </div>
   </MDXProvider>
