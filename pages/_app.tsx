d63041353a643e6bb3c98db2e06ba8f86c5e581c
Totolici Alexandru <totoalex62@gmail.com>
2024-01-19 15:48:20 +0200

    19 jan v3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,19 @@ import { MDXProvider } from '@mdx-js/react'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <MDXProvider components={{
+	return <>
+		<Head>
+			<link href="https://app.meetfox.com/assets/styles/popup.css" rel="stylesheet" />
+			<script src="https://app.meetfox.com/assets/libs/popup.min.js" type="text/javascript"></script>
+		</Head>
+		<MDXProvider components={{
 
-  }}>
-    <Head>
-      <link href="https://app.meetfox.com/assets/styles/popup.css" rel="stylesheet" />
-      <script src="https://app.meetfox.com/assets/libs/popup.min.js" type="text/javascript"></script>
-    </Head>
-    <div>
-      <Component {...pageProps} />
-    </div>
-  </MDXProvider>
+		}}>
+			<div>
+				<Component {...pageProps} />
+			</div>
+		</MDXProvider>
+	</>
 }
 
 export default MyApp
