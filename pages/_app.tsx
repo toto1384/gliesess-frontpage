45de300d0c000f71ae4bcca597c7a1bf3657e53a
Totolici Alexandru <totoalex62@gmail.com>
2024-01-27 18:49:12 +0200

    27 jan v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Footer } from '../components/footer'
 import { NextSeo } from 'next-seo'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <>
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 			title='Gliesess - SEO and Web Development Agency'
 			description='With a foundation built on 7 years of experience, we bring the technological expertise to the table. From the latest web development frameworks to SEO best practices, we stay at the forefront of technology trends. Our commitment to continuous learning ensures that your digital solutions are not only current but future-proof.'
 		></NextSeo>
+		<SpeedInsights />
 		<Head>
 			<link href="https://app.meetfox.com/assets/styles/popup.css" rel="stylesheet" />
 			<script src="https://app.meetfox.com/assets/libs/popup.min.js" type="text/javascript"></script>
